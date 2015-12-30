0a327f8b8faf88df3eff2d1cf6da8a415ef46435
Brendan Molloy <brendan@bbqsrc.net>
2015-12-30 18:32:25 +1100

    bump to v0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name = "pytoport",
-    version = "0.3.0a1",
+    version = "0.3.0",
     packages = find_packages(),
     author = "Brendan Molloy",
     author_email = "brendan+pypi@bbqsrc.net",
